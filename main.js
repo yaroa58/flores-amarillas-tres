e683e746ebebf09a78bbe88e9e2fa27ca6e9399a
yeison roa <alexandr.ortega@hotmail.com>
2026-03-08 11:32:32 -0500

    Cambio de musica para movil

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-onload = () =>{
+window.onload = () => {
+
     document.body.classList.remove("container");
+
+    const audio = document.getElementById("musica");
+
+    document.addEventListener("touchstart", function () {
+        audio.play();
+    }, { once: true });
+
+    document.addEventListener("click", function () {
+        audio.play();
+    }, { once: true });
+
 };
